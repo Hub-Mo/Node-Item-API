ac04505ae6393a4c3efec5058d823de4426cf434
Mo <Mohamed.elja@hotmail.com>
2024-01-22 21:45:23 +0100

    adds mongodb database

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const dbUri = 'mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.y4jhlbf.mongodb.net/Node-API?retryWrites=true&w=majority'
 
 //routes
 app.get('/', (req, res) => {
     res.send('Hello world and other planets')
 })
 
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000')
-})
 
+
+mongoose.connect('mongodb+srv://admin:admin@nodeapi.y4jhlbf.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000')
+    })
+    console.log('connected to mongo-db');
+}).catch((error) => {
+    console.log(error);
+})
